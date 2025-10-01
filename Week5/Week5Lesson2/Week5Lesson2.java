eb5a90383bd28463d8d0e661f14e7c4b25e87d47
Cyril Chan <kwunming0911@gmail.com>
2025-10-01 11:22:51 +0100

    Recursive function completed

--- Week5/Week5Lesson2/Week5Lesson2.java
+++ Week5/Week5Lesson2/Week5Lesson2.java
@@ -8,10 +8,10 @@
 public class Week5Lesson2 {
     public static void main(String[] args) {
         Week5Lesson2 w5l2 = new Week5Lesson2();
-        System.out.println(w5l2.isModThree(98));
-        System.out.println(w5l2.multiply(2,10));
-        System.out.println(w5l2.fib(4));
-
+//        System.out.println(w5l2.isModThree(98));
+//        System.out.println(w5l2.multiply(2,10));
+//        System.out.println(w5l2.fib(4));
+        System.out.println(w5l2.recursiveMultiply(4, -10));
     }
 
     // Method which returns true if a number is divisible by 3 and
@@ -24,11 +24,29 @@ public class Week5Lesson2 {
     // numbers without using the inbuilt * sign
     public int multiply(int n, int by) {
         int sum = 0;
-        while (by > 0){
+        while (by > 0) {
             by--;
             sum += n;
         }
         return sum;
+    }
+
+    public int recursiveMultiply(int n, int by) {
+        // logic: multiplication means multiple addition/reduction, by is the number of the operation
+        // keypoint, the starting point for multiple reduction should be -n and it should add by until it became 0.
+
+        if (by == 0) return 0;
+
+        // If by > 0, should keep returning function
+        if (by >= 1) return n + recursiveMultiply(n, by - 1);
+
+        // If by < 0
+        if (by < 0) {
+            return -n + recursiveMultiply(n, by + 1);
+        }
+
+        // End the recursion if by == 1
+        return n;
     }
 
     /*
@@ -42,12 +60,12 @@ public class Week5Lesson2 {
      * if we call fibonacci(4) we should get the value 3 returned.
      */
     public int fib(int term) {
-        if(term == 1 || term == 2) return 1;
+        if (term == 1 || term == 2) return 1;
 
         int cur = 1;
         int prev = 1;
         int next = cur + prev;
-        while(term > 2){
+        while (term > 2) {
             prev = cur;
             cur = next;
             next = cur + prev;
